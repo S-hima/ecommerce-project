c806c31529bd0360dc224b3b21734fe8a1145b90
Seemabhattarai <simabhattaraibca077@indrenicollege.edu.np>
2023-11-09 16:18:29 +0545

    done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Dashboard = () => {
+  const navigate =useNavigate();
+
+
+  const handlelogout =()=> {
+    navigate("/login");
+    //  toast("Sucessfully logout");
+
+
+
+}
+
+
+
   return (
     <div>WELCOME TO SEEMA'S WORLD
 
-<ToastContainer/>
+      <button onClick={handlelogout} className=" py-5  bg-primary hover:bg-opacity-90 shadow-soft-2xl mr-2 flex h-8 items-center w-full  justify-center bg-center stroke-0 text-center xl:py-5 text-white
+         focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5  ">logout</button>
+
+
+      
+
+        <ToastContainer/>
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { ToastContainer, } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Login = () => {
@@ -22,7 +24,7 @@ const Login = () => {
         axios.post('https://achyut.acetechnepal.com/login/', inputValue)
             .then(() => {
                 navigate("/dashboard");
-                toast("Form Submit Sucessfully");
+                toast("login Sucessfully");
                 
 
             })
@@ -84,11 +86,13 @@ const Login = () => {
                                 <button
                                     type="submit"
 
-                                    className=" py-5  bg-primary hover:bg-opacity-90 shadow-soft-2xl mr-2 flex h-8 items-center w-full  justify-center bg-center stroke-0 text-center xl:py-5 text-white  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5  "
+                                    className=" py-5  bg-primary hover:bg-opacity-90 shadow-soft-2xl mr-2 flex h-8 items-center w-full  justify-center bg-center stroke-0 text-center xl:py-5 text-white
+                                      focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5  "
                                 >
                                     
                                     Login
                                 </button>
+                                {/* <ToastContainer/> */}
                             </form>
                         </div>
                     </div>
